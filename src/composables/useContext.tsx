d0bfc85ef40eb0bae71673c18eca5bc43f5cad94
hungcurry <ooopp42@gmail.com>
2025-02-17 11:01:49 +0800

    fix useContext

--- src/composables/useContext.tsx
+++ src/composables/useContext.tsx
@@ -1,16 +1,29 @@
 import { createContext, useContext } from 'react'
 
-type TUser = {
+export type TProvidUser = {
   nickname: string
   setNickname: (nickname: string) => void
 }
-
-export const UserContext = createContext<TUser | null>(null)
-
+// 宣告Context 要放在全域
+export const UserContext = createContext<TProvidUser | null>(null)
 export function useUser() {
   const context = useContext(UserContext)
   if (!context) {
-    throw new Error('useUser must be used within a UserProvider')
+    throw new Error('useUser must be used within a Provider')
+  }
+  return context
+}
+// ----------------
+export type TProvidProduct = {
+  title: string
+  price: number
+}
+// 宣告Context 要放在全域
+export const ProductContext = createContext<TProvidProduct | null>(null)
+export function useProduct() {
+  const context = useContext(ProductContext)
+  if (!context) {
+    throw new Error('useProduct must be used within a Provider')
   }
   return context
 }
